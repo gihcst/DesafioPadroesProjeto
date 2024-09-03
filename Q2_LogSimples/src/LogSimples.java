a53cd46c3ca49362b08e77dea45639bc5c7e24ce
Naiumy dos Reis <139267691+Naiumydosreis@users.noreply.github.com>
2024-09-03 20:06:04 +0000

    finalizado

--- Q2_LogSimples/src/LogSimples.java
+++ Q2_LogSimples/src/LogSimples.java
@@ -6,11 +6,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class LogSimples implements Iterable<String>{
+    
+    private static LogSimples instance;
     private List<String> mensagens;
 
-    public LogSimples(){
+    private LogSimples(){
         mensagens = new LinkedList<>();
     }
+
+public static synchronized LogSimples getLogSimples(){
+    if (instance == null) {
+        instance = new LogSimples();
+    }
+    return instance;
+}
 
     public void log(String m){
         String logM = LocalDate.now().toString() + " : " + m;
