68cb165f39692babb1bda4845dce67de88a656b7
Gio <132241659+gihcst@users.noreply.github.com>
2024-09-29 04:48:28 +0000

    Testando todos os validadores rs

--- Q1_ValidadorDeCampos/src/App.java
+++ Q1_ValidadorDeCampos/src/App.java
@@ -1,12 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String email = "bernardo.copsteindvmucrs.br";
+        String inteiro = "15652";
+        String matricula = "23111285-5";
         IValidador validador = FValidador.fabricaValidador(Tipos.EMAIL);
+        IValidador validadorInt = FValidador.fabricaValidador(Tipos.INTEIRO);
+        IValidador validadorMatricula = FValidador.fabricaValidador(Tipos.MATRICULA);
 
         if (validador.valida(email)) {
             System.out.println(email + " é um email válido!");
         } else {
             System.out.println(email + " não é um email válido!");
         }
+
+        if (validadorInt.valida(inteiro)){
+            System.out.println(inteiro + " É UM INT!");
+        } else {
+            System.out.println("NÃO É UM INT >:(");
+        }
+
+        if (validadorMatricula.valida(matricula)){
+            System.out.println(matricula + " É UMA MATRICULA!");
+        } else {
+            System.out.println("NÃO É UMA MATRICULA >:(");
+        }
+
     }
 }
